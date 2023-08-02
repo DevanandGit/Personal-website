f26847133abf862660c840db39c200324375639a
DevanandGit <nandhudevanand4419@gmail.com>
2023-08-02 20:58:42 +0530

    new ommit

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1%kfvx964#==l&k&x4nai^c#1%-chme6-l3n_owfc6b%4=s@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
